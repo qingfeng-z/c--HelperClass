430448dd1859fb6b14cfad7000b6d8d573598a82
清风 <qingfeng@shouhouyi.net>
2019-10-17 11:19:54 +0800

    init

---
+++ HelperClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelperClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
